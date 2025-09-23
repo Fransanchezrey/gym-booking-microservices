4ca0bf6896dd3d4404a18200374f6ee4ef98687c
Fran Sánchez Rey <franciscosanchezrey@gmail.com>
2025-09-23 11:49:32 +0200

    Primeros pasos

---
+++ class-catalog-service/src/main/java/com/gymbooking/classcatalog/ClassCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gymbooking.classcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassCatalogServiceApplication.class, args);
+	}
+
+}
---
+++ class-catalog-service/src/test/java/com/gymbooking/classcatalog/ClassCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gymbooking.classcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
