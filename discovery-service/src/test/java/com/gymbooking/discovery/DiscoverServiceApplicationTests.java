54c88f6558a438056d1e76d07cbf7ed8ce93d1a3
Fran Sánchez Rey <franciscosanchezrey@gmail.com>
2025-10-02 14:14:19 +0200

    service-discovery con Eureka api-gateway

---
+++ discovery-service/src/test/java/com/gymbooking/discovery/DiscoverServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gymbooking.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
